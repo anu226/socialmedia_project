1eaf4783dfe9e601b6f0ae166d806a5fbd03592d
Anamika Gupta <anu200022g@gmail.com>
2021-01-09 16:27:39 +0530

    added home.ejs file

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,13 @@
-module.exports.home= function(req,res){
-    return res.end('<h1> Express is up for codeial</h1>');
-}
-//module.exports.actionName = function(req,res){}
+// //commented becoz it would directly render the output instead of going to views
+// module.exports.home= function(req,res){
+//     return res.end('<h1> Express is up for codeial</h1>');
+// };
+//////////module.exports.actionName = function(req,res){}//////////////format of controller
 module.exports.page = function(req,res){
-    return res.end("yayyy page has opened");
+       return res.end("yayyy page has opened");
+    };
+module.exports.home= function(req,res){
+    return res.render('home',{
+        title:" Codeial Home"
+    });
 }
