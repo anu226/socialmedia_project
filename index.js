a1d7be1092b2f2b9d5f0544f81cf770a112fb2d1
Anamika Gupta <anu200022g@gmail.com>
2021-01-14 00:01:22 +0530

    extracting style and scripts from sub pages

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+//calling static fun for assets
+app.use(express.static('./assets')); 
+//use layouts
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(expressLayouts);
+//extract style and scripts from sub pages into the layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 //use express router
 app.use('/',require('./routes'));
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`server is runnning on port: ${port}`);
 });
+//actually when i wrote the layout then it is giving error
